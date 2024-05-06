4bb52319a1de8372db7ba291559b5c723dd2825f
Uperpelli <C836817374@colostate.edu>
2024-05-05 18:50:34 -0600

    Added validation for login and sign up

--- FinalProject/src/mainclasses/HomeScreen.java
+++ FinalProject/src/mainclasses/HomeScreen.java
@@ -4,11 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -29,32 +26,29 @@ public class HomeScreen extends JFrame implements ActionListener {
 	private JTextField txtPassword;
 
 //	JComboBox cmbUserType;
-	
+
 	private JButton btnLogin;
 	private JButton btnSignUp;
-	
+
 	Client client;
 	User user;
-	
-	
+
 	public HomeScreen(Client client) {
 
 		this.client = client;
-		
-		
+
 		initializeUIComponents();
 		doTheLayout();
-		
+
 		this.btnLogin.addActionListener(this);
 		this.btnSignUp.addActionListener(this);
-		
-		
+
 		this.setTitle("Home Screen");
-        //this.setSize(600, 600);
+		// this.setSize(600, 600);
 		this.pack();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null); // Center the window
-        
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null); // Center the window
+
 	}
 
 	public void initializeUIComponents() {
@@ -63,115 +57,126 @@ public class HomeScreen extends JFrame implements ActionListener {
 		this.lblPassword = new JLabel("Password");
 		this.lblUserType = new JLabel("User Type");
 		this.lblSignUpText = new JLabel("Don't have an account?");
-		
+
 		this.txtUsername = new JTextField(10);
 		this.txtPassword = new JTextField(10);
-		
+
 //		String userTypes[] = {"","Customer","Chef", "DBA"};
 //		this.cmbUserType = new JComboBox<String>(userTypes);
 //		this.cmbUserType.setSelectedIndex(1);
-		
+
 		this.btnLogin = new JButton("Login");
 		this.btnSignUp = new JButton("Sign Up");
-		
-		
+
 	}
-	
+
 	public void doTheLayout() {
 		final JPanel row1 = new JPanel();
 		final JPanel row2 = new JPanel();
 		final JPanel row3 = new JPanel();
 		final JPanel row4 = new JPanel();
 		final JPanel row5 = new JPanel();
-		
-		
+
 		final JPanel center = new JPanel();
 		final JPanel bottom = new JPanel();
-		
+
 		row1.add(this.lblUsername);
 		row1.add(this.txtUsername);
-		
+
 		row2.add(this.lblPassword);
 		row2.add(this.txtPassword);
-		
+
 //		row3.add(this.lblUserType);
 //		row3.add(this.cmbUserType);
-		
+
 		row4.add(this.btnLogin);
-		
+
 		row5.add(this.lblSignUpText);
 		row5.add(this.btnSignUp);
-		
-		center.setLayout(new GridLayout(4,1));
+
+		center.setLayout(new GridLayout(4, 1));
 		center.add(row1);
 		center.add(row2);
 //		center.add(row3);
 		center.add(row4);
 		center.add(row5);
-		
-		
-		
-		
+
 		this.setLayout(new BorderLayout());
 		this.add(new JPanel(), BorderLayout.NORTH);
-		this.add(center,  BorderLayout.CENTER);
+		this.add(center, BorderLayout.CENTER);
 		this.add(bottom, BorderLayout.SOUTH);
-		
-		
+
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() == this.btnLogin) {
+		if (e.getSource() == this.btnLogin) {
 			LoginButtonClicked();
-		}else if(e.getSource() == this.btnSignUp) {
+		} else if (e.getSource() == this.btnSignUp) {
 			SignUpScreen signup = new SignUpScreen(this.client);
 			signup.setVisible(true);
 			dispose();
 		}
-			
+
 	}
-	
-	public void LoginButtonClicked(){
+
+	public void LoginButtonClicked() {
 		System.out.println("LoginButtonClicked");
 		try {
+			if (!validateFeilds())
+				return;
+
 			String username = this.txtUsername.getText();
 			String password = this.txtPassword.getText();
-			//TODO
-			//validations
-			
-			this.user = new User(0,username,"","", password, null,2,"");
-			
+
+			this.user = new User(0, username, "", "", password, null, 2, "");
+
 			this.user = (User) this.client.performAction(this.user);
-			
-			if(this.user!=null && this.user.getOptType()>0) {
+
+			if (this.user != null && this.user.getOptType() > 0) {
 				System.out.println("Message = " + this.user.getMessage());
-				
+
 				UserType uType = this.user.getUserType();
-				//System.out.println(uType.toString());
-				
-				if(uType.equals(UserType.ADMIN)) {
-					DbaScreen dbaScreen = new DbaScreen( this.client);
+				// System.out.println(uType.toString());
+
+				if (uType.equals(UserType.ADMIN)) {
+					DbaScreen dbaScreen = new DbaScreen(this.client);
 					dbaScreen.setVisible(true);
 					dispose();
-				}else if(uType.equals(UserType.CHEF)) {
+				} else if (uType.equals(UserType.CHEF)) {
 					ChefScreen chefScreen = new ChefScreen(this.client);
-		            chefScreen.setVisible(true);
-		            dispose();
-				}else if(uType.equals(UserType.USER)) {
-					CustomerScreen customerScreen = new CustomerScreen( this.client);
+					chefScreen.setVisible(true);
+					dispose();
+				} else if (uType.equals(UserType.USER)) {
+					CustomerScreen customerScreen = new CustomerScreen(this.client);
 					customerScreen.setVisible(true);
 					dispose();
 				}
-				
-			}else {
+
+			} else {
 				JOptionPane.showMessageDialog(null, "Invalid Credentials. Please try again");
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public boolean validateFeilds() {
+		String username = this.txtUsername.getText();
+		if (username.isBlank()) {
+			JOptionPane.showMessageDialog(null, "Please enter the username", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		String password = this.txtPassword.getText();
+		if (password.isBlank()) {
+			JOptionPane.showMessageDialog(null, "Please enter the username", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		return true;
 	}
 
 }
--- FinalProject/src/mainclasses/SignUpScreen.java
+++ FinalProject/src/mainclasses/SignUpScreen.java
@@ -22,81 +22,78 @@ import utility.Constants.UserType;
 
 public class SignUpScreen extends JFrame implements ActionListener {
 
-	
 	private JLabel lblUserName;
 	private JLabel lblFirstName;
 	private JLabel lblLastName;
 	private JLabel lblPassword;
 	private JLabel lblConfirmPassword;
-	
+
 	private JTextField txtUserName;
 	private JTextField txtFirstName;
 	private JTextField txtLastName;
 	private JTextField txtPassword;
 	private JTextField txtConfirmPassword;
-	
+
 	private JLabel lblUserType;
 	private JComboBox cmbUserType;
-	
+
 	JButton btnSignUp;
-	
+
 	User user;
 	Client client;
-	
+
 	public SignUpScreen(Client client) {
-		
+
 		this.client = client;
-		
+
 		initializeUIComponents();
 		doTheLayout();
-		
+
 		this.btnSignUp.addActionListener(this);
-		
+
 		this.setTitle("SignUp Screen");
-        //this.setSize(400, 400);
+		// this.setSize(400, 400);
 		this.pack();
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocationRelativeTo(null); // Center the window
-        
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null); // Center the window
+
 	}
-	
+
 	public void initializeUIComponents() {
-		
+
 		this.lblFirstName = new JLabel("First Name");
 		this.lblLastName = new JLabel("Last Name");
 		this.lblUserName = new JLabel("Username");
 		this.lblPassword = new JLabel("Password");
 		this.lblConfirmPassword = new JLabel("Confirm Password");
-		
-		
+
 		this.txtFirstName = new JTextField(10);
 		this.txtLastName = new JTextField(10);
 		this.txtUserName = new JTextField(10);
 		this.txtPassword = new JTextField(10);
 		this.txtConfirmPassword = new JTextField(10);
-		
+
 		this.btnSignUp = new JButton("Sign Up");
-		
+
 		this.lblUserType = new JLabel("User Type");
-		String [] userTypes =  getUserTypes();
+		String[] userTypes = getUserTypes();
 		this.cmbUserType = new JComboBox<String>(userTypes);
 		this.cmbUserType.setSelectedIndex(1);
-		
+
 	}
-	
+
 	public String[] getUserTypes() {
 		Constants.UserType[] userTypes = Constants.UserType.values();
 		String[] userTypeStrings = new String[userTypes.length];
 		for (int i = 0; i < userTypes.length; i++) {
 			userTypeStrings[i] = userTypes[i].toString();
-        }
+		}
 		System.out.println(userTypeStrings.toString());
 		return userTypeStrings;
 	}
-	
-	
+
 	public void doTheLayout() {
-		
+
 		final JPanel row1 = new JPanel();
 		final JPanel row2 = new JPanel();
 		final JPanel row3 = new JPanel();
@@ -105,28 +102,28 @@ public class SignUpScreen extends JFrame implements ActionListener {
 		final JPanel row6 = new JPanel();
 		final JPanel row7 = new JPanel();
 		final JPanel center = new JPanel();
-		
+
 		row1.add(this.lblFirstName);
 		row1.add(this.txtFirstName);
-		
+
 		row2.add(this.lblLastName);
 		row2.add(this.txtLastName);
-		
+
 		row3.add(this.lblUserName);
 		row3.add(this.txtUserName);
-		
+
 		row4.add(this.lblPassword);
 		row4.add(this.txtPassword);
-		
+
 		row5.add(this.lblConfirmPassword);
 		row5.add(this.txtConfirmPassword);
-		
+
 		row6.add(this.btnSignUp);
-		
+
 		row7.add(this.lblUserType);
 		row7.add(this.cmbUserType);
-		
-		center.setLayout(new GridLayout(7,1));
+
+		center.setLayout(new GridLayout(7, 1));
 		center.add(row1);
 		center.add(row2);
 		center.add(row3);
@@ -134,52 +131,95 @@ public class SignUpScreen extends JFrame implements ActionListener {
 		center.add(row5);
 		center.add(row7);
 		center.add(row6);
-		
+
 		this.setLayout(new BorderLayout());
 		this.add(center, BorderLayout.CENTER);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() == this.btnSignUp) {
+		if (e.getSource() == this.btnSignUp) {
 			SignUpButtonClicked();
 		}
 	}
 
 	public void SignUpButtonClicked() {
-		//TODO
+		// TODO
 		System.out.println("SignUpButtonClicked");
 		try {
+
+			if (!validateFields())
+				return;
+
 			String firstname = this.txtFirstName.getText();
 			String lastname = this.txtLastName.getText();
 			String pass = this.txtPassword.getText();
 			String username = this.txtUserName.getText();
 			String userType = this.cmbUserType.getSelectedItem().toString();
-			//TODO 
-			//validations
 			UserType type = UserType.valueOf(userType);
-			this.user = new User(0,username,firstname, lastname, pass, type, 1, "" );
-			
+			this.user = new User(0, username, firstname, lastname, pass, type, 1, "");
+
 			this.user = (User) this.client.performAction(this.user);
-			
-			if(this.user!=null && this.user.getOptType()>0) {
+
+			if (this.user != null && this.user.getOptType() > 0) {
 				System.out.println("Message = " + user.getMessage());
 				JOptionPane.showMessageDialog(null, "User Created succesfully.");
-				
+
 				HomeScreen hs = new HomeScreen(this.client);
 				hs.setVisible(true);
 				dispose();
-			}else {
+			} else {
 				JOptionPane.showMessageDialog(null, "User Creation Failed");
 			}
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		
-		
+	}
+
+	public boolean validateFields() {
+
+		String firstname = this.txtFirstName.getText();
+		if (firstname.isBlank()) {
+			JOptionPane.showMessageDialog(null, "Please enter the first name", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		if (firstname.length() > 45) {
+			JOptionPane.showMessageDialog(null, "First name should be less than 45", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		String lastname = this.txtLastName.getText();
+		if (lastname.isBlank()) {
+			JOptionPane.showMessageDialog(null, "Please enter the last name", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		if (lastname.length() > 45) {
+			JOptionPane.showMessageDialog(null, "Last name should be less than 45", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		String username = this.txtUserName.getText();
+		if (username.isBlank()) {
+			JOptionPane.showMessageDialog(null, "Please enter the username", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		if (username.length() >= 45) {
+			JOptionPane.showMessageDialog(null, "Username should be less than 45", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		String password = this.txtPassword.getText();
+		String confirmPassword = this.txtConfirmPassword.getText();
+
+		if (!password.equals(confirmPassword)) {
+			JOptionPane.showMessageDialog(null, "Password is not matching", null, JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+
+		return true;
 	}
 }
